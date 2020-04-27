6c0f0855e40b8aeb13dddf0400585f0be3223467
brodianskyi <pb82zaxe@studserv.uni-leipzig.de>
2020-04-27 20:21:29 +0200

    add number of data sets to the main map

--- src/app/modules/rechner/rechner.component.ts
+++ src/app/modules/rechner/rechner.component.ts
@@ -7,19 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RechnerComponent {
   daten_domains_arr = new Array(2);
-  current_data_domain: number = 1;
-  current_data_set: number = 1;
-  current_variables: number = 1;
   data_sets_arr = new Array(1);
   variables_arr = new Array(1);
+  data_domain_id: number = 1; 
   //{"# of data domain", "number of data sets in this data domain"}
   // new Map([[1, Map([[Array_data_set(1), Array_variables(1)],..]),..])
-  data_domain_map = new Map([[1, Array(1).fill(3)], [2, Array(1).fill(3)]]);
+  data_domain_map = new Map([[1, [3]], [2, [3]]]);
 
   constructor() { }
 
 
   number_data_domains(n_data_domains: number) {
+    this.daten_domains_arr.length = n_data_domains;
     let data_domain_map = new Map();
     let map_length = Number(n_data_domains) + 1;
     for (let i = 1; i < map_length; i++) {
@@ -31,29 +30,35 @@ export class RechnerComponent {
 
   number_data_sets(data_domain: number, n_data_sets: number) {
     console.log("data_domain", data_domain, "number data sets", n_data_sets);
+    this.data_domain_id = data_domain;
+    this.data_sets_arr.length = n_data_sets;
     let arr = new Array();
     arr.length = n_data_sets;
     // number of variables by default = 3
     arr.fill(3);
     this.data_domain_map.set(data_domain, arr);
     console.log("data_set_map", this.data_domain_map);
-    //this.current_data_domain = data_domain;
     //this.data_sets_arr.length = n_data_sets;
   }
 
   number_variables(n_domain: number, n_data_set: number, n_variables: number) {
-    console.log("n_domain", n_domain, "n_data_set", n_data_set, "n_variables", n_variables);
-    let arr = this.data_domain_map[n_domain]
+    console.log("#domain", n_domain, "#n_data_set", n_data_set+1, "kol_vo_variables", n_variables);
+    console.log("data_set_map",  this.data_domain_map);
+    // let arr = this.data_domain_map[n_domain];
+    let arr = this.data_domain_map.get(n_domain);
     console.log("array in variables", arr);
-    arr.length = n_data_set;
-    console.log("change length of arr", arr.length);
-    arr = arr.fill(n_variables)
+    // arr.length = n_data_set;
+    console.log("change length of arr to", arr.length);
+    arr[n_data_set] = n_variables;
+    //arr = arr.fill(n_variables)
     console.log("final array", arr);
     this.data_domain_map.set(n_domain, arr)
-
+    console.log("data_set_map_new",  this.data_domain_map);
     // this.variables_arr.length = n_variables;
   }
 
-
+  track_domain_key(index: any, daten_domain: any) {
+       return index;   
+  } 
 
 }
